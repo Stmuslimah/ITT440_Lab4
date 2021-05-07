fd2b57ca699f2d0959e4fd4a5b8b2bac6e84fd45
Stmuslimah <muslimahaisyah39@gmail.com>
2021-05-07 19:07:03 +0000

    Change coding

---
+++ Server4.4.c
@@ -0,0 +1,91 @@
+#include<stdio.h>
+#include<netdb.h>
+#include<netinet/in.h>
+#include<stdlib.h>
+#include<string.h>	//strlen
+#include<sys/socket.h>
+#include<sys/types.h>
+//#include<arpa/inet.h>	//inet_addr
+#include<unistd.h>	//write
+
+
+void func(int socket_desc)
+{
+	char buff[80];
+	int n;
+
+
+	for (;;) 
+	{
+		bzero(buff,80);
+
+
+		read(socket_desc, buff, sizeof(buff));
+		printf("From client %s\t\n To client : ", buff);
+		bzero(buff, 80);
+		n=0;
+		while ((buff[n++] = getchar()) != '\n');
+		write(socket_desc, buff, sizeof(buff));
+		if (strncmp("exit", buff, 4) == 0)
+		{
+			printf("Server exit...\n");
+			break;
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int socket_desc;
+	struct sockaddr_in server, client;
+	int conn;
+	int len;
+
+	socket_desc = socket (AF_INET , SOCK_STREAM , 0);
+	if (socket_desc  == -1)
+	{
+		printf("Could not create socket...\n");
+	}
+
+	else
+	bzero(&server, sizeof(server));
+
+	server.sin_family = AF_INET;
+	server.sin_port = htons(8888);
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+
+
+	
+
+	if(( bind(socket_desc, (struct sockaddr *)&server, sizeof(server))) != 0)
+	{
+		exit(0);
+	}
+	else
+		printf("Successfull\n");
+		
+
+
+	if((listen(socket_desc,5)) != 0)
+	{
+		printf("Listen failed...\n");
+		exit(0);
+	} 
+	else
+		printf("Server listening...\n");
+	len = sizeof(client);
+
+
+	conn = accept(socket_desc, (struct sockaddr *)&client, &len);
+	if(conn < 0)
+	{
+		printf("Server accept failed...\n");
+		exit(0);
+	}
+	else
+		printf("Server accept the client...\n");
+
+	func(conn); 
+
+	close(socket_desc);
+}
